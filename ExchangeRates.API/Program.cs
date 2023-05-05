1dedd47b190698deef2f79c8dcf8c94521f3e3d1
omari321 <O_pirtskhalaishvili@cu.edu.ge>
2023-05-06 01:46:22 +0400

    mc

--- ExchangeRates.API/Program.cs
+++ ExchangeRates.API/Program.cs
@@ -1,11 +1,19 @@
+using ExchangeRates.Application;
+using ExchangeRates.Infrastructure;
+using ExchangeRates.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Host.AddSettingsConfiguration();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
+builder.Services.AddApplication();
+builder.Services.AddDbContext();
 var app = builder.Build();
 
 //todo rate limiting
